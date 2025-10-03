9b179f367941db521078c30612fd5260af8bf30e
william <williamrutynamacedo@gmail.com>
2025-10-02 21:17:24 -0300

    ajustes, ainda falta seção de preços

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter >
+      <BrowserRouter basename="/HR/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "./",
+  base: '/HR/',
   plugins: [
     react(),
     mode === 'development' &&
